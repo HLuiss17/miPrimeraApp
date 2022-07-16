dba2c54d1c4627a2e98f544afedad5ff711bf6fe
HLuiss17 <106353117+HLuiss17@users.noreply.github.com>
2022-07-16 10:24:16 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 
 export default function App() {
+  //la logica del codigo
+  //definicion de hook de estado
+  const{numero1,setNumero1}=useState("");
+  const{numero2,setNumero2}=useState("");
+  const{resultado,setResultado}=useState("");
+  //funcion tipo flecha
+  const sumar=()=>{
+  resultado=numero1+numero2;
+   Alert.alert('la suma de num1 y num2 es'+resultado);
+  };
+  //Return es la vista
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Hola Luis</Text>
       <StatusBar style="auto" />
       <TextInput
-        placeolder="Ingrese un numero"
-        style={styles.imput}
+        style={styles.input}
+        onChangeText={(value)=>setNumero1(value)}
+        value={numero1}
+        placeholder="Ingrese el primer valor"
+        keyboardType="numeric"
       />
       <TextInput
-        placeolder="Ingrese un numero"
-        style={styles.imput}
+         style={styles.input}
+         onChangeText={(value)=>setNumero2(value)}
+         value={numero2}
+         placeholder="Ingrese el segundo valor"
+         keyboardType="numeric"
       />
       <Button
-        color="red"
-        title="Resolver"
+      color="blue"
+        title="Sumar"
+        onPress={sumar}
       />
     </View>
   );
@@ -32,7 +51,7 @@ const styles = StyleSheet.create({
   texto: {
     fontSize: 50,
   },
-  textInput:{
+  input:{
     borderWidth:1,
     borderColor:'gray',
     width:'80%',
@@ -47,4 +66,6 @@ const styles = StyleSheet.create({
   //suma,resta,m,d
   // ir al navegador react native
   //https://reactnative.dev/docs/textinput
+  //deber
+  //SUMA RESTA MULTIPLICACION Y DIVICION
 });
